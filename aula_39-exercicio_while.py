45cfe4d8167bc059b4eda0d945a65a0414510201
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-19 15:55:16 -0300

    Solução do exercício
    65 -  aula_39- Solução do exercício guiado com while

--- aula_39-exercicio_while.py
+++ aula_39-exercicio_while.py
@@ -3,9 +3,24 @@
 '''
 
 #   012345678910
+'''
 nome = 'Luiz Otávio' # Iteráveis
 
 tamanho_nome = len(nome)
 print(nome)
 print(tamanho_nome)
 print(nome[3])
+'''
+
+nome = 'Luiz Gustavo'
+I = 0
+novo_nome = ''
+
+
+while I < len(nome):
+    letra = nome[I]
+    novo_nome += f'*{letra}'
+    print(nome[I])
+    I += 1
+novo_nome += '*'    
+print(novo_nome)    
